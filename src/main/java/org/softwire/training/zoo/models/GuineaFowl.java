77d27bdddaf2def0aa5406742687969b96e5cf35
Lekshmi <skumarlekshmi@gmail.com>
2022-11-09 14:40:41 +0000

    Guinea Fowl can be groomed

--- src/main/java/org/softwire/training/zoo/models/GuineaFowl.java
+++ src/main/java/org/softwire/training/zoo/models/GuineaFowl.java
@@ -4,11 +4,21 @@ import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class GuineaFowl extends AbstractAnimal implements LargeAnimal{
+public class GuineaFowl extends AbstractAnimal implements LargeAnimal, CanBeGroomed{
     private LocalDateTime lastGroomed;
 
     public GuineaFowl (LocalDate dateOfBirth){
         super(dateOfBirth);
+    }
+
+    @Override
+    public void groom(){
+        lastGroomed = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString(){
+        return MessageFormat.format("{0}; Last Groomed {1}", super.toString(), lastGroomed);
     }
 
 
